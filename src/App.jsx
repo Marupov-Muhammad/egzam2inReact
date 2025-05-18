ecb049708798e02c6b397a35cb762c9df0856b87
Muhammad <mmarupov27@gmail.com>
2025-05-18 14:22:41 +0500

    maga

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './footer/footer'
+import Header from './header/header'
+import Section from './section/section'
+
+export default function App() {
+  return <>
+    <div className='div1'>
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  </>
+}
